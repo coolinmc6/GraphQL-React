7ec7adbc2100d3bc389bca17013357f834828fe7
Colin McNamara <coolinmc6@gmail.com>
2018-05-01 13:02:47 -0400

    Finish L23

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -6,17 +6,41 @@ const {
 	GraphQLObjectType,
 	GraphQLString,
 	GraphQLInt,
-	GraphQLSchema
+	GraphQLSchema,
+	GraphQLList
 
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+	name: 'Company', 
+	fields: () => ({
+		id: { type: GraphQLString},
+		name: { type: GraphQLString},
+		description: { type: GraphQLString},
+		users: {
+			type: new GraphQLList(UserType),
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+					.then(res => res.data);
+			}
+		}
+	})
+});
+
 const UserType = new GraphQLObjectType({
 	name: 'User', 
-	fields: {
+	fields: () => ({
 		id:  {type: GraphQLString},
 		firstName: {type: GraphQLString},
-		age: {type: GraphQLInt}
-	}
+		age: {type: GraphQLInt},
+		company: {
+			type: CompanyType,
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+					.then(res =>res.data);
+			}
+		}
+	})
 });
 
 
@@ -31,6 +55,15 @@ const RootQuery = new GraphQLObjectType({
 				// we just need to make sure to return a raw JS object or JSON
 				// return _.find(users, { id: args.id }); => used for static data
 				return axios.get(`http://localhost:3000/users/${args.id}`)
+					.then(resp => resp.data);
+			}
+		},
+		company: {
+			type: CompanyType,
+			args: { id: {type: GraphQLString}},
+			resolve(parentValue, args) {
+
+				return axios.get(`http://localhost:3000/companies/${args.id}`)
 					.then(resp => resp.data);
 			}
 		}
