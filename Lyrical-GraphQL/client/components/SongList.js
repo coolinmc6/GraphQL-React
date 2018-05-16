2df847cbb12a9f5aca465b640d281fc0eeba7c63
Colin McNamara <coolinmc6@gmail.com>
2018-05-16 10:06:56 -0400

    Finish L60

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -5,11 +5,24 @@ import { Link } from 'react-router';
 import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+	
+	onSongDelete(id) {
+		this.props.mutate({ variables: { id } })
+			.then(() => this.props.data.refetch())
+	}
+
 	renderSongs() {
-		return this.props.data.songs.map(song => {
+		// destructure the song object
+		return this.props.data.songs.map(({id, title}) => {
 			return (
-				<li key={song.id} className="collection-item">
-					{song.title}
+				<li key={id} className="collection-item">
+				<Link to={`/songs/${id}`}>
+					{title}
+				</Link>
+					<i
+						className="material-icons"
+						onClick={() => this.onSongDelete(id)}
+					>delete</i>
 				</li>
 			)
 		})	
@@ -28,4 +41,14 @@ class SongList extends Component {
 	}
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+	mutation DeleteSong($id: ID) {
+	  deleteSong(id: $id) {
+	    id
+	  }
+	}
+`;
+
+export default graphql(mutation)(
+	graphql(query)(SongList)
+);
