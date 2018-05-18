af7f89d494ae9ebaadd272d2c0934efa761a7b30
Colin McNamara <coolinmc6@gmail.com>
2018-05-18 09:51:58 -0400

    Finish Lyric app

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
+/*
+You can build the optimisticResponse by looking at the network requests from the mutation
+that you are running. You can see the response you get back and largely copy what's there.
+
+*/
 class LyricList extends Component {
-	onLike(id) {
-		console.log(id);
+	onLike(id, likes) {
+		this.props.mutate({ 
+			variables: { id },
+			optimisticResponse: {
+				__typename: "Mutation",
+				likeLyric: {
+					id, 
+					__typename: 'LyricType',
+					likes: likes + 1
+				}
+			}
+		})
 	}
 	renderLyrics() {
-		return this.props.lyrics.map(({ id, content }) => {
+		return this.props.lyrics.map(({ id, content, likes }) => {
 			return (
 				<li key={id} className="collection-item">
 					{content}
-					<i className="material-icons"
-						onClick={() => this.onLike(id)}
-					>thumb_up</i>
+					<div className="vote-box">
+						<i className="material-icons"
+							onClick={() => this.onLike(id, likes)}
+						>thumb_up</i> { likes }
+					</div>
 				</li>
 			)
 		})
@@ -25,4 +44,13 @@ class LyricList extends Component {
 	}
 }
 
-export default LyricList;
+const mutation = gql`
+	mutation LikeLyric($id:ID) {
+	  likeLyric(id:$id) {
+	    id
+	    likes
+	  }
+	}
+`
+
+export default graphql(mutation)(LyricList);
